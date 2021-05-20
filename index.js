ebcf3107b05438b22db8d93cfbb4bcb718ef35c9
TrevorZylks <trevorzylks@gmail.com>
2021-05-20 11:48:28 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// creating cards
+const nameList = ["Jake", "John", "Jilpeth"];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+}
+
+writeCards(nameList, 'birthday'); 
+
+// decrementing
+
+function countDown(counting) {
+    let i = counting; 
+    while (i >= 0) {
+      console.log(i--);
+      debugger;
+    }
+}
+
+countDown(11);
